11d012cdfa5bc3dff1132d040130c60640f699df
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-05-27 23:27:02 -0600

    Aqui solo el poner el mensaje 5 veces osea copiar y pegar todo la verdad y cambiar el ultimo numero nada mas

--- Estudent_promedios/src/Main.java
+++ Estudent_promedios/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        Estudiante estudiante1 = new Estudiante();
+        Estudiante estudiante2 = new Estudiante();
+        Estudiante estudiante3 = new Estudiante();
+        Estudiante estudiante4 = new Estudiante();
+        Estudiante estudiante5 = new Estudiante();
     }
 }
