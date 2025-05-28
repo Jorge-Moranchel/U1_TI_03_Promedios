4cb193c9074bc73e2f5ae96018f1324893797c98
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-05-27 23:28:08 -0600

    Aqui es mas facil ya solo es copiar lo que tenia ya que me ayudo en explicarme mi compañero uriel y esta todo bien todo ready

--- Estudent_promedios/src/Main.java
+++ Estudent_promedios/src/Main.java
@@ -9,5 +9,16 @@ public class Main {
         Estudiante estudiante3 = new Estudiante();
         Estudiante estudiante4 = new Estudiante();
         Estudiante estudiante5 = new Estudiante();
+
+        System.out.println("=== Estudiante 1 ===");
+        System.out.print("Nombre: ");
+        estudiante1.setNombre(scanner.nextLine());
+        System.out.print("Matrícula: ");
+        estudiante1.setMatricula(scanner.nextLine());
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Nota " + (i + 1) + ": ");
+            estudiante1.setNota(i, scanner.nextDouble());
+        }
+        scanner.nextLine();
     }
 }
