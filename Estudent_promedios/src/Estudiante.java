c067a511f7b87971a2f0b649471fb4a336f2530e
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-05-27 23:24:47 -0600

    Aqui ponemos el getter and setter pero se pueden rapido con refactor y listo

--- Estudent_promedios/src/Estudiante.java
+++ Estudent_promedios/src/Estudiante.java
@@ -4,4 +4,28 @@ public class Estudiante {
     private String matricula;
     private double[] notas = new double[5];
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getNota(int posicion) {
+        return notas[posicion];
+    }
+
+    public void setNota(int posicion, double valor) {
+        notas[posicion] = valor;
+    }
+
 }   
